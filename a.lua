9658f6f13c1f8d8b3013f6e780de274052f20b39
VorTex <65015743+ascaxz@users.noreply.github.com>
2021-05-06 07:44:13 +0700

    Update a.lua

--- a.lua
+++ a.lua
@@ -82,7 +82,7 @@ Esp.Add = function(plr, root, col)
     Holder.Name.Font = Esp.Settings.Font;
     Holder.Box.Filled = false
     Holder.Box.Thickness = 1
-	Holder.OutlineBox.Color = col
+	Holder.OutlineBox.Color = Esp.Settings.BoxColor
 	Holder.OutlineBox.Filled = false
     Holder.OutlineBox.Thickness = 1
     Holder.OutlineHealth.Filled = false
@@ -181,6 +181,13 @@ Esp.UpdateTracerStart = function(pos)
     TracerStart = pos
     for i, v in next, Esp.Container do
         v.Tracer.From = pos
+    end
+end
+
+Esp.UpdateBoxColor = function(color)
+    Esp.Settings.BoxColor = color;
+    for i, v in next, Esp.Container do
+        v.OutlineBox.Color = color;
     end
 end
 
